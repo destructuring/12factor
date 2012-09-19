44278e308f46e17976db4eed177bb2537153e604
Tom Bombadil <amanibhavam@destructuring.org>
2012-09-19 11:26:25 -0700

    configure microwave for master env

--- nodes/master.rb
+++ nodes/master.rb
@@ -1,8 +1,8 @@
-run_list []
+run_list [ '12factors' ]
 platform "generic"
 platform_version "0.0.1"
 
 release_dir ENV['PWD']
-project_dir "XXX"
-home_dir "XXX"
+project_dir "/data/12factors"
+home_dir ENV['HOME']
 app_env "master"
